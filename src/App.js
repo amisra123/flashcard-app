e10ed36504c1c8b14334e67d7cbf5966fe1b6d5b
amisra123 <aisuryamisra@gmail.com>
2021-07-24 19:18:59 +0530

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import 'font-awesome/css/font-awesome.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav class='navbar navbar-fixed-top' role='navigation'>
+      <div class='container'>
+        <div class='navbar-header'>
+          <button
+            type='button'
+            class='navbar-toggle'
+            data-toggle='collapse'
+            data-target='#navbar-collapse'
+          >
+            <span
+              class='glyphicon glyphicon-menu-hamburger'
+              aria-hidden='true'
+            ></span>
+            <span class='sr-only'>Toggle navigation</span>
+            <span class='icon-bar'></span>
+            <span class='icon-bar'></span>
+            <span class='icon-bar'></span>
+          </button>
+
+          <a class='navbar-brand' href='#'></a>
+        </div>
+
+        <div class='collapse navbar-collapse' id='navbar-collapse'>
+          <ul class='nav navbar-nav navbar-right'>
+            <li>
+              <a href=''>Browse</a>
+            </li>
+            <li>
+              <a href=''>Learn</a>
+            </li>
+            <li>
+              <a href=''>Create</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
